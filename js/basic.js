2c419c5e7d1cfd43cc78dc0774f5547d658c2694
TomiM97 <tomi.maijala@student.lut.fi>
2024-09-08 14:46:39 +0300

    2/5 done

---
+++ js/basic.js
@@ -0,0 +1,18 @@
+if(document.readyState !== "loading") {
+    console.log("Document is ready!");
+    initializeCode();
+} else {
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("Document is ready after waiting!");
+        initializeCode();
+    })
+}
+
+function initializeCode() {
+    const sayHello = document.getElementById("my-button");
+
+    sayHello.addEventListener("click", function() {
+        console.log("hello world");
+    })
+    
+}
