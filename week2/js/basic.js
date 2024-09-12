5df4e0dddd61a04df66bfefef94b7bbc4cfe47c9
TomiM97 <tomi.maijala@student.lut.fi>
2024-09-13 00:19:11 +0300

    w2 p2

--- week2/js/basic.js
+++ week2/js/basic.js
@@ -22,20 +22,13 @@ function initializeCode() {
         const email = document.getElementById("input-email").value;
         const isAdmin = document.getElementById("input-admin").checked ? "X" : "-";
 
-        const table = document.getElementById("user-table").getElementsByTagName("tbody")[0];
-        const newRow = table.insertRow();
+        const existingRowIndex = findRowIndexByUsername(username);
 
-        const usernameCell = newRow.insertCell(0);
-        const emailCell = newRow.insertCell(1);
-        const adminCell = newRow.insertCell(2);
-
-        usernameCell.textContent = username;
-        emailCell.textContent = email;
-        adminCell.textContent = isAdmin;
-
-        document.getElementById("input-username").value = "";
-        document.getElementById("input-email").value = "";
-        document.getElementById("input-admin").checked = false;
+        if (existingRowIndex !== -1) {
+            modifyRow(existingRowIndex, username, email, isAdmin);
+        } else {
+            addNewRow(username, email, isAdmin);
+        }
 
     });
     
@@ -44,6 +37,44 @@ function initializeCode() {
         const tableBody = document.getElementById("user-table").getElementsByTagName("tbody")[0];
         tableBody.innerHTML = "";
     })
+}
+
+// Checks if there's a username already in use and returns the row index or -1 if nothing is found
+function findRowIndexByUsername(username) {
+    const table = document.getElementById("user-table").getElementsByTagName("tbody")[0];
+    const rows = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName("td");
+        if (cells[0].textContent === username) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+// Modifies a row
+function modifyRow(rowIndex, username, email, isAdmin) {
+    const table = document.getElementById("user-table").getElementsByTagName("tbody")[0];
+    const row = table.getElementsByTagName("tr")[rowIndex];
+
+    row.cells[0].textContent = username;
+    row.cells[1].textContent = email;
+    row.cells[2].textContent = isAdmin;
+}
+
+// Adds a row
+function addNewRow(username, email, isAdmin) {
+    const table = document.getElementById("user-table").getElementsByTagName("tbody")[0];
+    const newRow = table.insertRow();
+
+    const usernameCell = newRow.insertCell(0);
+    const emailCell = newRow.insertCell(1);
+    const adminCell = newRow.insertCell(2);
+
+    usernameCell.textContent = username;
+    emailCell.textContent = email;
+    adminCell.textContent = isAdmin;
 }
 
 // Example rows
